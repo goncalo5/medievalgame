8a992254f748808d6de3a6cbad96f65718869ce3
goncalo5 <goncalo.s.and@gmail.com>
2017-03-06 20:49:58 +0000

    continuation of market presentation

--- presentation/market.py
+++ presentation/market.py
@@ -13,5 +13,21 @@ class Market(object):
         # find offers
         Label(self.root, text='I want').grid(row=self.row_i + 1, column=self.column_i)
         Label(self.root, text="I'm offering").grid(row=self.row_i + 2, column=self.column_i)
+        self.want = StringVar()
+        self.offer = StringVar()
         for i, resource in enumerate(self.village.resources):
-            Label(self.root, text=resource.name).grid(row=self.row_i, column=self.column_i + 1 + i)
+            Label(self.root, text=resource.name).grid(row=self.row_i, column=self.column_i + i + 1)
+            self.b_want = Radiobutton(self.root, variable=self.want, value=resource.name)
+            self.b_want.grid(row=self.row_i + 1, column=self.column_i + i + 1)
+            self.b_offer = Radiobutton(self.root, variable=self.offer, value=resource.name)
+            self.b_offer.grid(row=self.row_i + 2, column=self.column_i + i + 1)
+
+        header = ['Receive', 'for', 'player', 'Duration', 'Ratio', 'Availability', 'Accept']
+        for i, name in enumerate(header):
+            Label(self.root, text=name).grid(row=self.row_i + 4, column=self.column_i + i)
+        self.button = Button(self.root, text='accept', command=self.accept)
+        self.button.grid(row=self.row_i + 5, column=self.column_i + 6)
+
+    def accept(self):
+        if self.want.get() != self.offer.get():
+            print 'ok'
